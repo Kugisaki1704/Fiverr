36ad87d003a9c40bc180ac99cca097971417e70d
Kugisaki1704 <sapnabora85@gmail.com>
2023-12-14 15:20:50 +0530

    added catCards

--- fullstack/src/pages/home/Home.jsx
+++ fullstack/src/pages/home/Home.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import "./Home.scss"
 import Featured from '../../components/featured/Featured';
+import TrustedBy from '../../components/trustedBy/TrustedBy';
+import Slide from '../../components/slide/Slide';
+import CatCard from '../../components/catCard/CatCard';
+import {cards} from "../../data";
+
+
+
 const Home = () => {
   return (
     <div className='home'>
       <Featured/>
+      <TrustedBy/>
+      <Slide>
+        {cards.map((card) => (
+          <div key={card.id} className='carousel-item'>
+            <CatCard item={card} />
+          </div>
+        ))}
+      </Slide>
     </div>
   );
 };
