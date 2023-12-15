b721cf59a2b618713518bd25cde58cd4ab4d5a30
Kugisaki1704 <sapnabora85@gmail.com>
2023-12-15 23:00:10 +0530

    added project card carousal

--- fullstack/src/pages/home/Home.jsx
+++ fullstack/src/pages/home/Home.jsx
@@ -5,7 +5,8 @@ import TrustedBy from '../../components/trustedBy/TrustedBy';
 import Slide from '../../components/slide/Slide';
 import CatCard from '../../components/catCard/CatCard';
 import {cards} from "../../data";
-
+import ProjectCard from '../../components/projectCard copy/ProjectCard';
+import { projects } from '../../data';
 
 
 const Home = () => {
@@ -58,6 +59,41 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <h1>fiverr business</h1>
+            <h1>A business solution designed for teams</h1>
+            <p>Upgrade to a curated experience packed with tools and benefits, dedicated to businesses</p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Connect to freelancers with proven business experience
+            </div>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Get matched with the perfect talent by a customer success manager
+            </div>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Manage teamwork and boost productivity with one powerful workspace
+            </div>
+            <button>Explore Liverr Business</button>
+          </div>
+          <div className="item">
+            <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_2.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624768/business-desktop-870-x2.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <Slide>
+        {projects.map((card) => (
+          <div key={card.id} className='carousel-item'>
+            <ProjectCard item={card} />
+          </div>
+        ))}
+      </Slide>
     </div>
   );
 };
