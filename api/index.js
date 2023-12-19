79ae30fa6442d28c57625f0c991f10d66315a9fd
Kugisaki1704 <sapnabora85@gmail.com>
2023-12-19 21:57:43 +0530

    created index file for the api

---
+++ api/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+
+const app = express()
+dotenv.config();
+
+mongoose.set('strictQuery',true);
+
+const connect = async()=>{
+  try {
+      await mongoose.connect(process.env.MONGO);
+      console.log("Connected to Mongodb!");
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+app.use("/api/users",userRoute);
+
+app.listen(8800, ()=>{
+  connect();
+    console.log("Backend server is running!");
+})
