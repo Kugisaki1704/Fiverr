ff1175f6a87c7ee57cdf8915db258bfce751e5b7
Kugisaki1704 <sapnabora85@gmail.com>
2023-12-21 22:31:36 +0530

    added routes to index

--- api/index.js
+++ api/index.js
@@ -2,11 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
 
 const app = express()
 dotenv.config();
 
 mongoose.set('strictQuery',true);
+app.use(express.json());
 
 const connect = async()=>{
   try {
@@ -17,7 +24,14 @@ const connect = async()=>{
     }
 };
 
+
 app.use("/api/users",userRoute);
+app.use("/api/auth",authRoute);
+app.use("/api/gigs",gigRoute);
+app.use("/api/orders",orderRoute);
+app.use("/api/conversations",conversationRoute);
+app.use("/api/messages",messageRoute);
+app.use("/api/reviews",reviewRoute);
 
 app.listen(8800, ()=>{
   connect();
