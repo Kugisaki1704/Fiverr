a3178fed4b8737a14d561c09fde79f37e876e6d2
Kugisaki1704 <sapnabora85@gmail.com>
2023-12-25 21:02:17 +0530

    added jwt token

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,37 +1,49 @@
-
-import User from "../modules/user.model.js";
+import User from "../models/user.model.js";
+import createError from "../utils/createError.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export const register = async (req,res)=>{
+export const register = async (req, res, next) => {
+  try {
+    const hash = bcrypt.hashSync(req.body.password, 5);
+    const newUser = new User({
+      ...req.body,
+      password: hash,
+    });
 
-    try{
-        const hash = bcrypt.hashSync(req.body.password , 5);
-        const newUser = new User({
-            ...req.body,
-            password:hash,
-        });
-
-        await newUser.save(); 
-        res.status(201).send("User has been created!"); //succesfull creation of user
-    }catch(err){
-        res.status(500).send("Something went wrong!")
-    }
-    
+    await newUser.save();
+    res.status(201).send("User has been created.");
+  } catch (err) {
+    next(err);
+  }
 };
 
-
-export const login = async (req,res)=>{
-    try{
-        const user = await User.findOne({ username:req.body.username });
-        if(!user) return res.status(404).send("User not found!");
-
-        const isCorrect = bcrypt.compareSync(req.body.password , user.password);
-        if(!isCorrect) return res.status(400).send("Wrong password or Username!");
-
-        const {password , ...info} = user._doc;
-        res.status(200).send(info);
-    }catch(err)
-    {
-        res.status(500).send("Something went wrong!");
+export const login = async (req, res, next) => {
+    try {
+      const user = await User.findOne({ username: req.body.username });
+  
+      if (!user) return next(createError(404, "User not found!"));
+  
+      const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+      if (!isCorrect)
+        return next(createError(400, "Wrong password or username!"));
+  
+      const token = jwt.sign(
+        {
+          id: user._id,
+          isSeller: user.isSeller,
+        },
+        process.env.JWT_KEY
+      );
+  
+      const { password, ...info } = user._doc;
+      res
+        .cookie("accessToken", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .send(info);
+    } catch (err) {
+      next(err);
     }
-};
+  };
