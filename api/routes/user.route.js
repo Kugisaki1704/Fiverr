e64fa2c9fa0480768425d909793e14ee5505e169
Kugisaki1704 <sapnabora85@gmail.com>
2023-12-26 10:55:46 +0530

    added getUser

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,19 +1,10 @@
 import express from "express";
-<<<<<<< HEAD
-import { deleteUser, getUser } from "../controllers/user.controller.js";
+import { deleteUser, getUser } from "../controller/user.controller.js";
 import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
 router.delete("/:id", verifyToken, deleteUser);
 router.get("/:id", getUser);
-=======
-import {deleteUser} from "../controller/user.controller.js";
-
-const router = express.Router();
-
-router.delete("/:id", deleteUser);
-
->>>>>>> aff9519910aa01923830c111f93764e4f3f9054a
 
 export default router;
